f5e02722c4ba0d52bd1ce6b78747d2eba7890587
Ryan Kuck <ryankuck@gmail.com>
2019-07-06 16:39:18 -0400

    (2.1) header cleanup

--- c_version/my_memory.h
+++ c_version/my_memory.h
@@ -21,24 +21,9 @@ int space_used();
 
 
 // --- Pointer Logic --- //
-
 void divide_block(struct malloc_stc * block, int size);
 struct malloc_stc * get_first_block();
 struct malloc_stc * reset_first_block();
-
-// Meta Data Setters
-// void set_end(void * block, bool end);
-// void set_free(void * block, bool free);
-// void set_size(void * block, int size);
-// void set_next(void * block, void * next);
-// void set_prev(void * block, void * prev);
-
-// // Meta Data Getters
-// bool is_end(void * block);
-// bool is_free(void * block);
-// int get_size(void * block);
-// void * next_block(void * block);
-// void * prev_block(void * block);
 
 /* optional for debugging */
 void print_blocks();		
